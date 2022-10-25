a46aa579b5cf23fc35ed469166be7e986c0a629f
Tomasz Myszak <76054684+TomaszMyszak@users.noreply.github.com>
2022-10-25 15:04:58 +0200

    Add project files.

---
+++ SecurytyManagmentSystem/sms.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace SecurytyManagmentSystem
+{
+    partial class sms
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(sms));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btn_fireProtectionOn = new System.Windows.Forms.Button();
+            this.btn_cctv = new System.Windows.Forms.Button();
+            this.btn_DoorControl = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.gB_weather = new System.Windows.Forms.GroupBox();
+            this.gB_Alarm = new System.Windows.Forms.GroupBox();
+            this.gB_Task = new System.Windows.Forms.GroupBox();
+            this.picIcon = new System.Windows.Forms.PictureBox();
+            this.labConditions = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labTemp = new System.Windows.Forms.Label();
+            this.labPressure = new System.Windows.Forms.Label();
+            this.labWindSpeed = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.gB_weather.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "biurowiec.png");
+            this.imageList1.Images.SetKeyName(1, "budTechniczny.png");
+            this.imageList1.Images.SetKeyName(2, "calosc.png");
+            // 
+            // btn_fireProtectionOn
+            // 
+            resources.ApplyResources(this.btn_fireProtectionOn, "btn_fireProtectionOn");
+            this.btn_fireProtectionOn.Name = "btn_fireProtectionOn";
+            this.btn_fireProtectionOn.UseVisualStyleBackColor = true;
+            // 
+            // btn_cctv
+            // 
+            resources.ApplyResources(this.btn_cctv, "btn_cctv");
+            this.btn_cctv.Name = "btn_cctv";
+            this.btn_cctv.UseVisualStyleBackColor = true;
+            // 
+            // btn_DoorControl
+            // 
+            resources.ApplyResources(this.btn_DoorControl, "btn_DoorControl");
+            this.btn_DoorControl.Name = "btn_DoorControl";
+            this.btn_DoorControl.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Image = global::SecurytyManagmentSystem.Properties.Resources.zdjecieGlowne;
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // gB_weather
+            // 
+            this.gB_weather.Controls.Add(this.label7);
+            this.gB_weather.Controls.Add(this.labConditions);
+            this.gB_weather.Controls.Add(this.label2);
+            this.gB_weather.Controls.Add(this.picIcon);
+            this.gB_weather.Controls.Add(this.labTemp);
+            this.gB_weather.Controls.Add(this.label5);
+            this.gB_weather.Controls.Add(this.label3);
+            this.gB_weather.Controls.Add(this.labWindSpeed);
+            this.gB_weather.Controls.Add(this.labPressure);
+            resources.ApplyResources(this.gB_weather, "gB_weather");
+            this.gB_weather.Name = "gB_weather";
+            this.gB_weather.TabStop = false;
+            // 
+            // gB_Alarm
+            // 
+            resources.ApplyResources(this.gB_Alarm, "gB_Alarm");
+            this.gB_Alarm.Name = "gB_Alarm";
+            this.gB_Alarm.TabStop = false;
+            // 
+            // gB_Task
+            // 
+            resources.ApplyResources(this.gB_Task, "gB_Task");
+            this.gB_Task.Name = "gB_Task";
+            this.gB_Task.TabStop = false;
+            // 
+            // picIcon
+            // 
+            this.picIcon.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.picIcon, "picIcon");
+            this.picIcon.Name = "picIcon";
+            this.picIcon.TabStop = false;
+            // 
+            // labConditions
+            // 
+            resources.ApplyResources(this.labConditions, "labConditions");
+            this.labConditions.BackColor = System.Drawing.Color.Transparent;
+            this.labConditions.ForeColor = System.Drawing.Color.Black;
+            this.labConditions.Name = "labConditions";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Name = "label7";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Name = "label3";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Name = "label5";
+            // 
+            // labTemp
+            // 
+            resources.ApplyResources(this.labTemp, "labTemp");
+            this.labTemp.BackColor = System.Drawing.Color.Transparent;
+            this.labTemp.ForeColor = System.Drawing.Color.Black;
+            this.labTemp.Name = "labTemp";
+            // 
+            // labPressure
+            // 
+            resources.ApplyResources(this.labPressure, "labPressure");
+            this.labPressure.BackColor = System.Drawing.Color.Transparent;
+            this.labPressure.ForeColor = System.Drawing.Color.Black;
+            this.labPressure.Name = "labPressure";
+            // 
+            // labWindSpeed
+            // 
+            resources.ApplyResources(this.labWindSpeed, "labWindSpeed");
+            this.labWindSpeed.BackColor = System.Drawing.Color.Transparent;
+            this.labWindSpeed.ForeColor = System.Drawing.Color.Black;
+            this.labWindSpeed.Name = "labWindSpeed";
+            // 
+            // sms
+            // 
+            this.AllowDrop = true;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gB_Task);
+            this.Controls.Add(this.gB_Alarm);
+            this.Controls.Add(this.gB_weather);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_DoorControl);
+            this.Controls.Add(this.btn_cctv);
+            this.Controls.Add(this.btn_fireProtectionOn);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "sms";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.gB_weather.ResumeLayout(false);
+            this.gB_weather.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picIcon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_fireProtectionOn;
+        private System.Windows.Forms.Button btn_cctv;
+        private System.Windows.Forms.Button btn_DoorControl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox gB_weather;
+        private System.Windows.Forms.GroupBox gB_Alarm;
+        private System.Windows.Forms.GroupBox gB_Task;
+        private System.Windows.Forms.PictureBox picIcon;
+        private System.Windows.Forms.Label labConditions;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labTemp;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labWindSpeed;
+        private System.Windows.Forms.Label labPressure;
+    }
+}
+
---
+++ SecurytyManagmentSystem/sms.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+using pogoda;
+
+
+
+namespace SecurytyManagmentSystem
+{
+    public partial class sms : Form
+    {
+        public sms()
+        {
+            InitializeComponent();
+            ///użyjemy metody getWeather() do pobrania danych pogodowych
+            getWeather();
+        }
+
+        
+
+
+        
+        void getWeather()
+        {
+
+            /// <summary>
+            /// ta metoda pobiera dane pogodowe z serwisu pogodowego z pliku JSON i przypisuje je do odpowiednich zmiennych
+            /// </summary>
+            /// <param name="url">https://api.openweathermap.org/data/2.5/weather?q=Katowice&appid=6c6141b28f4f15cd91e691b478cf268a</param>
+            using (WebClient web = new WebClient())
+            {
+                string APIkey = "6c6141b28f4f15cd91e691b478cf268a";
+                string url = String.Format("https://api.openweathermap.org/data/2.5/weather?q=" + "Tarnowskie Góry" + "&appid=" + APIkey); //wstawiamy nazwę miasta(0) i klucz API (1)
+
+                string json = web.DownloadString(url);                                                                                  /// <summary>
+                                                                                                                                        ///pobieramy dane z serwisu pogodowego w formacie JSON
+                WetherInfo.root Info = JsonConvert.DeserializeObject<WetherInfo.root>(json);                                         ///przypisujemy pobrane dane do zmiennej root
+                labConditions.Text = Info.weather[0].main;                                                                           ///przypisujemy pobrane dane do zmiennej description
+                
+                labPressure.Text = Info.main.pressure.ToString() + " hPa";                                                               ///przypisujemy pobrane dane do zmiennej pressure
+                labWindSpeed.Text = Info.wind.speed.ToString() + " m/s";                                                               ///przypisujemy pobrane dane do zmiennej speed
+                labTemp.Text = Info.main.temp.ToString() + " °K";                                                                     ///przypisujemy pobrane dane do zmiennej temp
+
+                picIcon.ImageLocation = "https://openweathermap.org/img/w/" + Info.weather[0].icon + ".png";
+            }
+
+          
+        }
+        
+
+    }
+}
+
+
